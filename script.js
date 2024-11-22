f3415f46bd0a479fdb697ecd66156a5871865cf0
ipon30882 <ipon_dd@windowslive.com>
2024-11-22 15:23:47 +0700

    Login screen

---
+++ script.js
@@ -0,0 +1,308 @@
+function showAlert() {
+    var password = document.getElementById("password").value
+
+    
+    
+    if (password == "PublishThisL)istPlease") {
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "root"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "!@"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "wubao"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "password"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123456"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "admin"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "12345"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1234"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "p@ssw0rd"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "jiamima"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "test"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "root123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "!"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "!q@w"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "!qaz@wsx"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "idc!@"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "admin!@"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == ""){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "alpine"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "qwerty"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "12345678"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "111111"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123456789"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1q2w3e4r"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "default"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1234567"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "qwe123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1qaz2wsx"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1234567890"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "abcd1234"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "000000"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "user"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "toor"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "qwer1234"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1q2w3e"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "asdf1234"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "redhat"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1234qwer"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "cisco"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "12qwaszx"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "test123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1q2w3e4r5t"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "admin123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "changeme"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1qazxsw2"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123qweasd"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "q1w2e3r4"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "letmein"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "server"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "root1234"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "master"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "abc123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "rootroot"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "a"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "system"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "pass"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1qaz2wsx3edc"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "p@$$w0rd"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "112233"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "welcome"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "!QAZ2wsx"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "linux"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123321"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "manager"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1qazXSW@"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "q1w2e3r4t5"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "oracle"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "asd123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "admin123456"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "ubnt"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123qwe"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "qazwsxedc"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "administrator"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "superuser"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "zaq12wsx"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "121212"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "654321"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "ubuntu"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "0000"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "zxcvbnm"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "root@123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "1111"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "vmware"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "q1w2e3"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "qwerty123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "cisco123"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "11111111"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "pa55w0rd"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "asdfgh"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "11111"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "123abc"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "asdf"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "centos"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "888888"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "54321"){
+        alert("This Password Is Weak.") 
+    }   
+    if (password == "password123"){
+        alert("This Password Is Weak.") 
+    }  
+
+}   
+       
